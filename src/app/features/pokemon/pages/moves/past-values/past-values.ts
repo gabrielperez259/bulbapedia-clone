823bce0a69c3b060a62de1f2494b8ddc9c62a954
gabrielperez259 <gabrielperez259@hotmail.com>
2026-09-04 08:27:35 -0300

    styles(past-values): css refactor

--- src/app/features/pokemon/pages/moves/past-values/past-values.ts
+++ src/app/features/pokemon/pages/moves/past-values/past-values.ts
@@ -1,17 +1,16 @@
 import { Component, inject, input } from '@angular/core';
-import { MoveDetails, PastMoveValues } from '../../../models/moves/move-details';
+import { MoveDetails } from '../../../models/moves/move-details';
 import { MovesDataClient } from '../../../services/moves-data-client';
+import { CleanTextPipe } from "../../../../../shared/pipes/clean-text.pipe";
+import { MoveListItem } from '../../../components/move-list-item/move-list-item';
 
 @Component({
   selector: 'app-past-values',
-  imports: [],
+  imports: [CleanTextPipe],
   templateUrl: './past-values.html',
   styleUrl: './past-values.scss',
 })
-export class PastValues {
-  
-  pastValues = input.required<MoveDetails['past_values']>();
+export class PastValues {  
   movesDataDetails = inject(MovesDataClient)
-
 
 }
